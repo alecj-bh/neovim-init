200b9f0d09a08e45c391773fc1d3aed6fc52a303
Alec <alec.j@bluehimmel.com>
2025-01-30 00:40:12 +0000

    Coq, Iron, Telescope

--- keymaps.lua
+++ keymaps.lua
@@ -5,19 +5,16 @@ vim.g.floaterm_position = 'topright'
 vim.g.floaterm_keymap_new    = '<leader>fw'
 vim.g.floaterm_keymap_prev   = '<leader>fp'
 vim.g.floaterm_keymap_next   = '<leader>fn'
-vim.g.floaterm_keymap_toggle = '<leader>ft'
+vim.g.floaterm_keymap_toggle = '<leader>F'
 
 vim.keymap.set("n", "<c-j>", "<c-w>j")
 vim.keymap.set("n", "<c-k>", "<c-w>k")
 vim.keymap.set("n", "<c-h>", "<c-w>h")
 vim.keymap.set("n", "<c-l>", "<c-w>l")
-vim.keymap.set("n", "<leader>N", ":NvimTreeToggle<CR>")
+vim.keymap.set("n", "<leader>N", ":CHADopen<CR>")
 vim.keymap.set("n", "<leader>D", ":bd<CR>")
 
-vim.keymap.set("n", "<leader>mn", require("nvim-tree.api").marks.navigate.next)
-vim.keymap.set("n", "<leader>mp", require("nvim-tree.api").marks.navigate.prev)
-vim.keymap.set("n", "<leader>ms", require("nvim-tree.api").marks.navigate.select)
-
+--[[
 vim.keymap.set("n", "<leader>cf", "<Plug>(coc-fix-current)")
 vim.keymap.set("n", "<leader>cac", "<Plug>(coc-codeaction-cursor)")
 vim.keymap.set("n", "<leader>cal", "<Plug>(coc-codeaction-line)")
@@ -28,13 +25,37 @@ vim.keymap.set("n", "<leader>car", "<Plug>(coc-codeaction-refactor)")
 vim.keymap.set("v", "<leader>carr", "<Plug>(coc-codeaction-refactor-selected)")
 
 vim.keymap.set("n", "<leader>cd", "<Plug>(coc-definition)")
-vim.keymap.set("n", "<leader>ce", "<Plug>(coc-declaration)")
+vim.keymap.set("n", "<leader>cdd", "<Plug>(coc-declaration)")
 vim.keymap.set("n", "<leader>ci", "<Plug>(coc-implementation)")
 vim.keymap.set("n", "<leader>ct", "<Plug>(coc-type-definition)")
 vim.keymap.set("n", "<leader>cr", "<Plug>(coc-references)")
 vim.keymap.set("n", "<leader>cu", "<Plug>(coc-references-used)")
 
-vim.keymap.set("n", "<leader>o", ":CocList --top outline<CR>")
+vim.keymap.set("n", "<leader>cen", "<Plug>(coc-diagnostic-next-error)")
+vim.keymap.set("n", "<leader>cep", "<Plug>(coc-diagnostic-prev-error)")
+]]
+
+--vim.keymap.set("n", "<leader>o", ":CocList --top outline<CR>")
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>tf', builtin.find_files)
+vim.keymap.set('n', '<leader>tg', builtin.live_grep)
+vim.keymap.set('n', '<leader>tb', builtin.builtin)
+vim.keymap.set('n', '<leader>th', builtin.help_tags)
+vim.keymap.set('n', '<leader>ts', function() builtin.lsp_workspace_symbols({ query = vim.fn.input("Query >") }) end)
+
+--Comes from nvim_metals plugin
+local function execute_command(command_params, callback)
+  vim.lsp.buf_request(0, "workspace/executeCommand", command_params, function(err, result, ctx)
+    if callback then
+      callback(err, ctx.method, result)
+    elseif err then
+      print(string.format("Could not execute command: %s", err.message))
+    end
+  end)
+end
+vim.keymap.set('n', '<leader>cr', function() execute_command({ command = "metals.generate-bsp-config" }) end)
+vim.keymap.set('n', '<leader>ci', function() execute_command({ command = "metals.build-import" }) end)
 
 vim.keymap.set("n", "]b", "<Plug>(cokeline-focus-next)")
 vim.keymap.set("n", "[b", "<Plug>(cokeline-focus-prev)")
--- plugins.lua
+++ plugins.lua
@@ -2,29 +2,65 @@ vim.cmd('filetype on')
 
 local Plug = vim.fn['plug#']
 vim.call('plug#begin')
-Plug "nvim-tree/nvim-web-devicons"
-Plug "nvim-tree/nvim-tree.lua"
+-- File browser
+Plug('ms-jpq/chadtree', {branch= 'chad', ['do']= 'python3 -m chadtree deps'})
+
+-- Status line
 Plug 'feline-nvim/feline.nvim'
 Plug 'nvim-lua/plenary.nvim'
+
+-- Buffer line
 Plug('willothy/nvim-cokeline', { branch= 'main' })
+
+-- Terminal 
 Plug 'voldikss/vim-floaterm'
-Plug('folke/tokyonight.nvim', { branch= 'main' })
-Plug 'kien/ctrlp.vim'
-Plug('neoclide/coc.nvim', {branch= 'release'})
-Plug 'lewis6991/gitsigns.nvim'
+
+-- Color and style
 Plug 'norcalli/nvim-colorizer.lua'
+Plug('folke/tokyonight.nvim', { branch= 'main' })
+Plug 'lewis6991/gitsigns.nvim'
+Plug 'nvim-tree/nvim-web-devicons'
+
+-- File finder
+-- Plug 'kien/ctrlp.vim'
+Plug('nvim-telescope/telescope.nvim', { branch= '0.1.x' }) -- brew insall ripgrip
+Plug('nvim-treesitter/nvim-treesitter', {['do']=':TSUpdate'})
+
+-- Git
 Plug 'tpope/vim-fugitive'
+
+-- LSP and completion
+-- Plug('neoclide/coc.nvim', {branch= 'release'})
+Plug('ms-jpq/coq_nvim', {branch= 'coq'})
+Plug('ms-jpq/coq.artifacts', {branch= 'artifacts'})
+Plug('ms-jpq/coq.thirdparty', {branch= '3p'})
+Plug('neovim/nvim-lspconfig')
+
+-- REPL
+Plug('Vigemus/iron.nvim')
+
 vim.call('plug#end')
 
-require("nvim-tree").setup({
-	actions = {
-		change_dir = {
-			global = true
-		}
-	}
-})
+--- COQ ----
+vim.g.coq_settings = {
+	auto_start = true
+}
+
+local lsp = require 'lspconfig'
+local coq = require "coq"
+
+lsp.jdtls.setup(coq.lsp_ensure_capabilities()) -- brew install jdtls
+lsp.metals.setup(coq.lsp_ensure_capabilities()) -- brew install coursier; coursier setup; coursier install metals
+lsp.pyright.setup{ settings = {python = {pythonPath = '/opt/homebrew/bin/python3.11'}}}
+
+require('coq_3p') {
+	{ src = "bc", short_name = "MATH", precision = 6 },
+	{ src = "nvimlua", short_name = "nLUA", conf_only = true },
+	{ src = "repl", sh = "zsh", shell = { p = "perl", n = "node"}, max_lines = 99, deadline = 500, unsafe = { "rm", "poweroff", "mv" } }
+}
 
 
+--- cokeline ---
 local is_picking_focus = require('cokeline.mappings').is_picking_focus
 local is_picking_close = require('cokeline.mappings').is_picking_close
 local get_hex = require('cokeline.hlgroups').get_hl_attr
@@ -98,3 +134,65 @@ require('colorizer').setup()
 
 vim.cmd[[colorscheme tokyonight]]
 
+--- Iron ----
+local iron = require("iron.core")
+
+iron.setup {
+  config = {
+    -- Whether a repl should be discarded or not
+    scratch_repl = true,
+    -- Your repl definitions come here
+    repl_definition = {
+      sh = {
+        -- Can be a table or a function that
+        -- returns a table (see below)
+        command = {"zsh"}
+      },
+      python = {
+        command = { "python3.11" },  -- or { "ipython", "--no-autoindent" }
+        format = require("iron.fts.common").bracketed_paste_python
+      }
+    },
+    -- How the repl window will be displayed
+    -- See below for more information
+    -- repl_open_cmd = require('iron.view').bottom(40),
+  },
+  -- Iron doesn't set keymaps by default anymore.
+  -- You can set them here or manually add keymaps to the functions in iron.core
+  keymaps = {
+    send_motion = "<space>sc",
+    visual_send = "<space>sc",
+    send_file = "<space>sf",
+    send_line = "<space>sl",
+    send_paragraph = "<space>sp",
+    send_until_cursor = "<space>su",
+    send_mark = "<space>sm",
+    mark_motion = "<space>mc",
+    mark_visual = "<space>mc",
+    remove_mark = "<space>md",
+    cr = "<space>s<cr>",
+    interrupt = "<space>s<space>",
+    exit = "<space>sq",
+    clear = "<space>cl",
+  },
+  -- If the highlight is on, you can change how it looks
+  -- For the available options, check nvim_set_hl
+  highlight = {
+    italic = true
+  },
+  ignore_blank_lines = true, -- ignore blank lines when sending visual select lines
+}
+
+vim.keymap.set('n', '<space>rs', '<cmd>IronRepl<cr>')
+vim.keymap.set('n', '<space>rr', '<cmd>IronRestart<cr>')
+vim.keymap.set('n', '<space>rf', '<cmd>IronFocus<cr>')
+vim.keymap.set('n', '<space>rh', '<cmd>IronHide<cr>')
+
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', {noremap = true, silent = false})
+
+--- Folding ---
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldnestmax = 3
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 0
